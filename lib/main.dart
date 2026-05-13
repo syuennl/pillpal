ae97bafdc8e9db2c4e556d37863aa11bc866dbe0
Li Syuen <syuen@syuens-mac.local>
2026-05-14 02:06:31 +0800

    Initial project setup

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/add_medicine_screen.dart';
+import 'screens/history_screen.dart';
+import 'screens/caregiver_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PillPal',
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(
+      //     seedColor: const Color(#8DB654),
+      //     primary: const Color(0xFF8DB654),
+      //   ),
+      //   useMaterial3: true,
+      //   fontFamily: 'Roboto', // Default standard font, can be changed later
+      // ),
+      // home: const HomeScreen(),
+      home: const HomeScreen(),
+    );
+  }
+}

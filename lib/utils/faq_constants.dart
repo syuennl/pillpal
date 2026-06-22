fad30f8def6df8b35182919f6304c053686ce93d
syuen <syuennl04@gmail.com>
2026-06-22 22:52:29 +0800

    chore: added faq constants for profile screen

---
+++ lib/utils/faq_constants.dart
@@ -0,0 +1,22 @@
+final List<Map<String, String>> faqConstants = [
+  {
+    'question': 'How do I add a medication?',
+    'answer':
+        'Tap the "+" button on the My Pillbox screen, then use your camera to scan the medication label. Our OCR technology will automatically extract the medication information.',
+  },
+  {
+    'question': 'What if I miss a dose?',
+    'answer':
+        'If you miss a dose, the app will mark it as "Missed" in your medication history. You can view your adherence history in the Diary tab.',
+  },
+  {
+    'question': 'How do I set up quiet hours?',
+    'answer':
+        'Go to Profile, scroll down to the "Quiet Hours" section, then set your start and end times to silence notifications during specific periods, like night time.',
+  },
+  {
+    'question': 'Is my health data secure?',
+    'answer':
+        'Yes, all your medication data is stored securely on your device. We do not share your personal health information with third parties.',
+  },
+];

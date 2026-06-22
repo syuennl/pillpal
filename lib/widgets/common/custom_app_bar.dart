1d36897f5e9dc64b95429ee2c69acf11dc96390f
syuen <syuennl04@gmail.com>
2026-06-22 22:51:03 +0800

    feat: added commonly used widgets across screens

---
+++ lib/widgets/common/custom_app_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_colours.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final String? subtitle;
+  final bool showBackButton;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.subtitle,
+    this.showBackButton = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      toolbarHeight: 90,
+      backgroundColor: AppColours.primaryGreen,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      title: Padding(
+        padding: EdgeInsets.fromLTRB(showBackButton ? 8 : 24, 28, 24, 28),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (showBackButton)
+              IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            if (showBackButton) const SizedBox(width: 8),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  if (subtitle != null) ...[
+                    const SizedBox(height: 4),
+                    Text(
+                      subtitle!,
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.9),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(90);
+}
---
+++ lib/widgets/common/dashboard_header.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_colours.dart';
+
+class DashboardHeader extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final VoidCallback onAddPressed;
+
+  const DashboardHeader({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.onAddPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: AppColours.primaryGreen,
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top + 24,
+        left: 24,
+        right: 24,
+        bottom: 24,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                subtitle,
+                style: const TextStyle(fontSize: 14, color: Colors.white),
+              ),
+            ],
+          ),
+
+          GestureDetector(
+            onTap: onAddPressed,
+            child: Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white.withOpacity(0.3),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    offset: const Offset(0, 10),
+                    blurRadius: 15,
+                    spreadRadius: -3,
+                  ),
+                ],
+              ),
+              child: const Icon(Icons.add, color: Colors.white, size: 24),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
